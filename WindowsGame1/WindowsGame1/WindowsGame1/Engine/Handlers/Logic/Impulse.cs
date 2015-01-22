22d89e1ba6481bd9dd827a79aae0f1fff141ffd5
Eternityis <ccarmo@optonline.net>
2015-01-22 03:53:03 -0500

    save

--- WindowsGame1/WindowsGame1/WindowsGame1/Engine/Handlers/Logic/Impulse.cs
+++ WindowsGame1/WindowsGame1/WindowsGame1/Engine/Handlers/Logic/Impulse.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
 
 namespace WindowsGame1.Engine.Handlers.Logic
     {
@@ -11,7 +14,9 @@ namespace WindowsGame1.Engine.Handlers.Logic
             public float heading;
        public float transX;
        public float transY;
+       public Vector2 compForces;
        public bool killMe = false;
+       private const float conversion = (float)57.2957795;
 
        public static void sudoku(Impulse i)
        {
@@ -24,5 +29,16 @@ namespace WindowsGame1.Engine.Handlers.Logic
             heading = h;
             Lists.impulseMasterList.Add(this); //add to master list for garbage collection when unneeded
         }
+
+       public Impulse(Vector2 vec) //takes input of forces as avector, (the y and x components already calculated) and determines the impulses direction and force in dir
+       {
+
+           compForces = vec;
+           heading = (float)Math.Atan2(vec.X, vec.Y)*conversion;
+           force = 
+       }
+
+
+
         }
     }
